3bf02fcf4ae2c3ac67cef78a54b1779701b758ce
Victor Marquina Cardenas <victor.marquina@sisol.gob.pe>
2019-01-03 17:47:28 -0500

    fix consulta fechas

--- get/get_compFechas.php
+++ get/get_compFechas.php
@@ -18,14 +18,15 @@ if ($mes!='*') {
 }
 $mesnombre=array('Al '.substr($fechaActualuzacion,0,3).substr($fechaActualuzacion,3,2),'ENERO','FEBRERO','MARZO','ABRIL','MAYO','JUNIO','JULIO','AGOSTO','SEPTIEMBRE','OCTUBRE','NOVIEMBRE','DICEMBRE');
 $resultado=$claseConsulta5->compFechas_ingresos($anio1,$anio2,$mes,$fechaActualuzacion);
-$resultado2=$claseConsulta5->compFechas_atenciones($anio1,$anio2,$mes,$fechaActualuzacion);
+$resultado2=$claseConsulta6->compFechas_atenciones($anio1,$anio2,$mes,$fechaActualuzacion);
 
 
 if (sizeof($resultado)>0 ){
   echo  "<table id='tb_response_fecha_I' class='table tablesorter table-bordered'>
     <thead>
     <tr>
-      <th rowspan='2'class='bg-silver f-s-12 text-center'>FECHA</th>
+      <th rowspan='2'class='bg-silver f-s-12 text-center'>DÍA</th>
+      <th rowspan='2'class='bg-silver f-s-12 text-center'>MES</th>
       <th colspan='2' class='bg-silver f-s-12 text-center'>".$anio1."</th>
       <th colspan='2' class='bg-silver f-s-12 text-center'>".$anio2."</th>
     </tr>
@@ -39,7 +40,8 @@ if (sizeof($resultado)>0 ){
     <tbody>";
  for ($i=0; $i <sizeof($resultado) ; $i++) {
    echo   "<tr>
-          <td class='p-3 f-s-11 text-center m-r-10 m-l-10'> ".$resultado[$i]['fecha']."</td>
+          <td class='p-3 f-s-11 text-center m-r-10 m-l-10'> ".substr($resultado[$i]['fecha'],0,2)."</td>
+          <td class='p-3 f-s-11 text-center m-r-10 m-l-10'> ".substr($resultado[$i]['fecha'],3,2)."</td>
           <td class='p-3 f-s-11 text-right m-r-10 m-l-10'>".number_format($resultado[$i][$anio1],2,'.',',')."</td>
           <td class='p-3 f-s-11 text-right m-r-10 m-l-10'>".number_format($resultado2[$i][$anio1],2,'.',',')."</td>
           <td class='p-3 f-s-11 text-right m-r-10 m-l-10'>".number_format($resultado[$i][$anio2],2,'.',',')."</td>
@@ -55,7 +57,7 @@ for ($i=0; $i <sizeof($resultado2) ; $i++) {
   $total4+=$resultado2[$i][$anio2];
 }
 echo "<tr>
-<td class='bg-silver p-3 f-s-11 text-left m-r-10 m-l-10'><strong>TOTAL</strong></td>
+<td colspan='2' class='bg-silver p-3 f-s-11 text-left m-r-10 m-l-10'><strong>TOTAL</strong></td>
 <td class='bg-silver p-3 f-s-11 text-right m-r-10 m-l-10'><strong>".number_format($total1,2,'.',',')."</strong></td>
 <td class='bg-silver p-3 f-s-11 text-right m-r-10 m-l-10'><strong>".number_format($total3,2,'.',',')."</strong></td>
 <td class='bg-silver p-3 f-s-11 text-right m-r-10 m-l-10'><strong>".number_format($total2,2,'.',',')."</strong></td>
